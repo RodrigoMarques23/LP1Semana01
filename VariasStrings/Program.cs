da88ef213d9c993ae833a5ef0fe8684b5bd515d2
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-02 15:12:23 +0000

    Novas Strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s= "Um tab\t e um emoji \u1F49 ";
+            string b= @"Isto \u1F49 não é um emoji";
+
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+        }
+    }
+}
