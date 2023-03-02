92d64fee6b6e14801ad03f24e379f0ad6cbb423a
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-02 14:54:19 +0000

    Int add

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int hexa = 0x10;
+            uint b = 0x10U;
+            int bin = 0b10110110;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(hexa);
+            Console.WriteLine(bin);
+        }
+    }
+}
