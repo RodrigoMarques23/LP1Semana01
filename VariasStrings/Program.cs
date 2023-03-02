c4f565cf3a514f5bbf993b8bc0437e8a22079b59
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-02 15:24:53 +0000

    Strings Vars Add

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,11 +6,23 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            string s= "Um tab\t e um emoji \u1F49 ";
-            string b= @"Isto \u1F49 não é um emoji";
+            int x = 5;
+            int y = 3;
+            string s = "Um tab\t e um emoji \u1F49 ";
+            string b = @"Isto \u1F49 não é um emoji";
+            string c = "ola" + "mundo";
+            string a = "Isto é um" + x + "(cinco)";
+            string u = String.Format("1+1={0}",2);
 
             Console.WriteLine(s);
             Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(a);
+            Console.WriteLine($"O Valor de x e' {x}");
+            Console.WriteLine($"O Valor de x + y e' {x + y}");
+            Console.WriteLine(u);
+            Console.WriteLine(@"Verbatim, X={0}",x);
+
         }
     }
 }
