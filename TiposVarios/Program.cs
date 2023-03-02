eae6dad068ba0b71c9f7f9e96c56857c3f3489e2
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-02 15:03:53 +0000

    Characters Add

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,11 +10,18 @@ namespace TiposVarios
             int hexa = 0x10;
             uint b = 0x10U;
             int bin = 0b10110110;
+            char emoji = '\u1F3D';
+            char emo2 = '\u2318';
+            char emo3 = '\u1F49';
 
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(hexa);
             Console.WriteLine(bin);
+            Console.WriteLine(emoji);
+            Console.WriteLine(emo2);
+            Console.WriteLine(emo3);
+
         }
     }
 }
