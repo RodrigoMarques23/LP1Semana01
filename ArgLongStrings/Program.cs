12e1294237846285b7743ab0607c9ca2896044b3
Butchsss <rodrigomarques23work@gmail.com>
2023-03-03 13:37:33 +0000

    ArgLongStrings ADD

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)//Run through every arg passed
+            {
+                if (arg.Length > 4)//verify if arg is longer than 4 chars
+                {
+                    string arg2 = arg.ToUpper();//transform to upper
+                    Console.WriteLine(arg2);//print arg
+                }
+            }
+        }
+    }
+}
